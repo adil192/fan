09f5e6b2eb5691a7d64d5ed1f9bbd35add2eb3b3
Adil Hanney <adilhanney@disroot.org>
2025-07-03 06:27:51 +0100

    feat: add theming code

---
+++ lib/components/themed_app.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ThemedApp extends StatelessWidget {
+  final String title;
+  final Widget home;
+
+  const ThemedApp({super.key, required this.title, required this.home});
+
+  @override
+  Widget build(BuildContext context) {
+    const primaryColor = Colors.deepOrange;
+
+    switch (Theme.of(context).platform) {
+      case TargetPlatform.iOS:
+      case TargetPlatform.macOS:
+        return CupertinoApp(
+          title: title,
+          theme: CupertinoThemeData(
+            brightness: Brightness.dark,
+            primaryColor: primaryColor,
+          ),
+          localizationsDelegates: [DefaultMaterialLocalizations.delegate],
+          home: home,
+        );
+      default:
+        return MaterialApp(
+          title: title,
+          theme: ThemeData.from(
+            colorScheme: ColorScheme.fromSeed(
+              brightness: Brightness.dark,
+              seedColor: primaryColor,
+            ),
+          ),
+          home: home,
+        );
+    }
+  }
+}
