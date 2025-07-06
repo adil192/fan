f525741f74e23b66f9b053a1eaa44d209b66e78d
Adil Hanney <adilhanney@disroot.org>
2025-07-06 17:24:02 +0100

    ref: move button styling to its own widget

--- lib/components/fan_controls.dart
+++ lib/components/fan_controls.dart
@@ -9,64 +9,87 @@ class FanControls extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return IconButtonTheme(
-      data: IconButtonThemeData(
-        style: IconButton.styleFrom(
-          iconSize: 48,
-          minimumSize: const Size(0, 48 * 3),
-        ),
-      ),
-      child: Center(
-        child: ConstrainedBox(
-          constraints: const BoxConstraints(maxWidth: 1200),
-          child: ListenableBuilder(
-            listenable: fanState,
-            builder: (context, _) {
-              return Row(
-                spacing: 12,
-                children: [
-                  Flexible(
-                    flex: 2,
-                    fit: FlexFit.tight,
-                    child: IconButton.filledTonal(
-                      onPressed: () {},
-                      icon: const Icon(Icons.abc),
-                    ),
+    return Center(
+      child: ConstrainedBox(
+        constraints: const BoxConstraints(maxWidth: 1200),
+        child: ListenableBuilder(
+          listenable: fanState,
+          builder: (context, _) {
+            return Row(
+              spacing: 12,
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: _FanControlsButton(
+                    onPressed: () {},
+                    active: false,
+                    icon: const Icon(Icons.abc),
                   ),
-                  Flexible(
-                    flex: 3,
-                    fit: FlexFit.tight,
-                    child: fanState.isOn
-                        ? IconButton.filled(
-                            onPressed: () => fanState.isOn = false,
-                            style: IconButton.styleFrom(
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(32),
-                              ),
-                            ),
-                            icon: const Icon(Symbols.mode_fan),
-                            tooltip: 'Turn off fan',
-                          )
-                        : IconButton.filledTonal(
-                            onPressed: () => fanState.isOn = true,
-                            icon: const Icon(Symbols.mode_fan_off),
-                            tooltip: 'Turn on fan',
-                          ),
+                ),
+                Expanded(
+                  flex: 3,
+                  child: _FanControlsButton(
+                    onPressed: () => fanState.isOn = !fanState.isOn,
+                    active: fanState.isOn,
+                    tooltip: fanState.isOn ? 'Turn off fan' : 'Turn on fan',
+                    icon: fanState.isOn
+                        ? const Icon(Symbols.mode_fan)
+                        : const Icon(Symbols.mode_fan_off),
                   ),
-                  Flexible(
-                    flex: 2,
-                    fit: FlexFit.tight,
-                    child: IconButton.filledTonal(
-                      onPressed: () {},
-                      icon: const Icon(Icons.abc),
-                    ),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: _FanControlsButton(
+                    onPressed: () {},
+                    active: false,
+                    icon: const Icon(Icons.abc),
                   ),
-                ],
-              );
-            },
-          ),
+                ),
+              ],
+            );
+          },
         ),
       ),
     );
+  }
+}
+
+class _FanControlsButton extends StatelessWidget {
+  const _FanControlsButton({
+    required this.onPressed,
+    this.active = false,
+    this.tooltip,
+    required this.icon,
+  });
+
+  final VoidCallback? onPressed;
+  final bool active;
+  final String? tooltip;
+  final Widget icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return active
+        ? IconButton.filled(
+            onPressed: onPressed,
+            style: IconButton.styleFrom(
+              iconSize: 48,
+              minimumSize: const Size(0, 48 * 3),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(32),
+              ),
+            ),
+            icon: icon,
+            tooltip: tooltip,
+          )
+        : IconButton.filledTonal(
+            onPressed: onPressed,
+            style: IconButton.styleFrom(
+              iconSize: 48,
+              minimumSize: const Size(0, 48 * 3),
+            ),
+            icon: icon,
+            tooltip: tooltip,
+          );
   }
 }
