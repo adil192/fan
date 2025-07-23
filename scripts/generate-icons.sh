8d97f4976f4debde2415c9bc91f8f1accb883437
Adil Hanney <adilhanney@disroot.org>
2025-07-23 11:12:05 +0100

    feat: add android monochrome icon

--- scripts/generate-icons.sh
+++ scripts/generate-icons.sh
@@ -3,6 +3,7 @@
 # Convert all svg to png
 for svg_file in assets/icon/*.svg; do
   png_file="${svg_file%.svg}.png"
+  echo "Rasterizing $svg_file..."
   rsvg-convert -o "$png_file" "$svg_file"
 done
 
