29f5ba904fd659792c1da88ef7d9e7a6b527d92c
Adil Hanney <adilhanney@disroot.org>
2025-08-04 16:21:23 +0100

    tweak: remove unused libmpv from android builds
    The mpv libs don't support 16KB page sizes coming soon to Android

--- lib/data/fan_noise_player.dart
+++ lib/data/fan_noise_player.dart
@@ -4,7 +4,6 @@ import 'dart:ui';
 
 import 'package:audio_session/audio_session.dart';
 import 'package:fan/data/fan_state.dart';
-import 'package:fan/data/stows.dart';
 import 'package:flutter/material.dart';
 import 'package:just_audio/just_audio.dart';
 import 'package:just_audio_media_kit/just_audio_media_kit.dart';
---
+++ patches/remove_libmpv.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# We remove the mediakit packages (which aren't used on Android) to
+# support 16 KB page sizes.
+#
+# Libraries that do not support 16 KB:
+# base/lib/arm64-v8a/libmedia_kit_native_event_loop.so
+# base/lib/arm64-v8a/libmediakitandroidhelper.so
+# base/lib/arm64-v8a/libmpv.so
+# base/lib/x86_64/libmedia_kit_native_event_loop.so
+# base/lib/x86_64/libmediakitandroidhelper.so
+# base/lib/x86_64/libmpv.so
+
+echo "Removing media_kit and related packages from pubspec.yaml"
+sed -i -e "/just_audio_media_kit/d" pubspec.yaml
+sed -i -e "/media_kit/d" pubspec.yaml
+
+echo "Removing JustAudioMediaKit usages"
+sed -i -e "/package:just_audio_media_kit/d" lib/data/fan_noise_player.dart
+sed -i -e "/JustAudioMediaKit.ensureInitialized()/d" lib/data/fan_noise_player.dart
